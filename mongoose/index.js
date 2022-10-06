74badb619fd3eed33ef4d830a8591c572475ab7e
ibozorus <korkmazie@gmail.com>
2022-10-06 19:23:34 +0200

    - Mongoose

---
+++ mongoose/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const conn = mongoose.connect(url);
+conn.then((db) => {
+    console.log('Conected to Server');
+
+    var newDish = Dishes({
+        name: 'Uthapizza',
+        description: 'test'
+    });
+    newDish.save().then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+        .then((dishes) => {
+            console.log(dishes);
+            return Dishes.remove({});
+        })
+        .then(() => {
+            return mongoose.conn.close()
+        })
+});
